a5b134c0b26da680d2266d283664c1f1a94cec88
yyymh123 <mye83@gatech.edu>
2024-09-09 19:19:16 -0400

    Modify the script so that it has 4 functions

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-query="$1"
-subject="$2"
-output="$3"
+query=$1
+subject=$2
+output=$3
 
-blastn -query "$query" -subject "$subject" -task blastn-short -outfmt "6 qseqid sseqid evalue pident length qlen" -out blast_results.txt
+tblastn -query "$query" -subject "$subject" -outfmt "6 qseqid sseqid pident length qlen" -out blast_results.txt
 
-awk '$4==100 && $5==$6' blast_results.txt > "$output"
+awk '{ if ($3 >= 30 && $4 >= 0.9 * $5) print $0 }' blast_results.txt > "$output"
 
+num_matches=$(wc -l < "$output")
+echo "Number of matches: $num_matches"
 
-num=$(wc -l < "$output")
-echo "Number of perfect matches: $num"
-
-rm blast_results.txt
